7deb2dadb0ce7dd49588ed70bca9245ac0b3a2bc
KimSanggi <sangiki07@gmail.com>
2018-09-22 11:35:15 +0900

    02.PyQt5 QDate, QTime, QDateTime Python

--- PyQt-Example2/exam02.py
+++ PyQt-Example2/exam02.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import QDateTime, QDate, QTime, Qt
+from PyQt5.QtCore import QDateTime, QDate, QTime, Qt, QCoreApplication
 
 
 
