1cff95b80c5befc0698a4b2097f87b2e24e607e4
KimSanggi <sangiki07@gmail.com>
2018-09-22 11:30:28 +0900

    PyQt5 QDate, QTime, QDateTime Python

---
+++ PyQt-Example2/exam02.py
@@ -0,0 +1,26 @@
+import sys
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QDateTime, QDate, QTime, Qt
+
+
+
+
+
+
+datetime = QDateTime.currentDateTime()
+print(datetime.toString())
+print(datetime.toString(Qt.ISODate))
+print(datetime.toString(Qt.DefaultLocaleLongDate))
+
+
+date = QDate.currentDate()
+print(date.toString())
+print(date.toString(Qt.ISODate))
+print(date.toString(Qt.DefaultLocaleLongDate))
+
+time = QTime.currentTime()
+print(time.toString())
+print(time.toString(Qt.ISODate))
+print(time.toString(Qt.DefaultLocaleLongDate))
+
