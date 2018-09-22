0dc849ec11ad3dc3225136044b80ac36fef09c1b
KimSanggi <sangiki07@gmail.com>
2018-09-22 11:35:57 +0900

    03.PyQt5 Converting Local Time To UTC Time Python

--- PyQt-Example2/exam03.py
+++ PyQt-Example2/exam03.py
@@ -10,7 +10,6 @@ from PyQt5.QtCore import QDateTime, QDate, QTime, Qt
 
 datetime = QDateTime.currentDateTime()
 
-
 print("Local Date And Time IS {}".format(datetime.toString(Qt.DefaultLocaleLongDate)))
 print("Universal Date And Time IS {}".format(datetime.toUTC().toString()))
 print("The Offset From UTC IS {0} : Seconds ".format(datetime.offsetFromUtc()))
